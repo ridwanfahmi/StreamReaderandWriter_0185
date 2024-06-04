212d6b0963a3b581f7af6138c3b6e2759301a019
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-04 10:15:57 +0700

    menampilkan tulisan >= Menulis file, \'q\' untuk keluar

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -10,4 +10,12 @@ int main() {
 
 	//unuk menyimpan data berbentuk string
 	string baris;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
