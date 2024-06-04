53e52d737d65a29dbd335c24011ecc7b10eff00f
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-04 11:25:22 +0700

    memperbaiki dan menyelesaikan project kedua

--- IOfile/IOfile.cpp
+++ IOfile/IOfile.cpp
@@ -12,41 +12,44 @@ int main() {
 
 	//membuka file dalam mode menulis
 	ofstream outfile;
-	//menunjuk kesebuah nama file
+	//menjuk ke sebuah nama file
 	outfile.open(NamaFile + ".txt", ios::out);
 
+	cout << ">= Menuli file, \'q\' untuk keluar" << endl;
 
-	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-	//unlimited loop untuk menulis
-	while (true)
-	{
+	//unlumited loop untuk menulis
+	while (true) {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
-		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
-	//selesai dalam menulis sekarang tutup filenya
+	// selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
-	//membuka file dalam mode membaca
+	// membuka file dalam mode membaca
 	ifstream infile;
 
-	//menunjuk ke sebuah file 
+	//menunjuk ke sebuah file
 	infile.open(NamaFile + ".txt", ios::in);
 
-	cout << endl << ">= Membuka dan membaca file " << endl;
-	//jika ada file maka
-	if (infile.is_open()) {
+	cout << endl << ">= Membuka dan membaca file" << endl;
+	//jika fila ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
 		while (getline(infile, baris))
 		{
+			// dan tampilkan di sini
 			cout << baris << '\n';
 		}
+		//tutup file tersebut setelah selesai
 		infile.close();
 	}
-	//jika tidak ditemukan file maka akan menampilkan ini
-	else
-		cout << "Unable to open file";
+	// jika tidak ditemukan file maka akan menampilkan ini 
+	else cout << "Unable to open file";
 	return 0;
 }
