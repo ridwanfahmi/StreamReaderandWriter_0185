6f071848b998d953c086d5178507a857b7a44822
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-04 11:12:14 +0700

    menyelesaikan project keempat

--- StandardException/StandardException.cpp
+++ StandardException/StandardException.cpp
@@ -6,5 +6,19 @@
 using namespace std;
 
 int main() {
-	
+	cout << "Awal Program" << endl; //penanda 1:....
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array int 3 elemen
+		//cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan objek exception
+		cout << e.what() << endl;
+		//akan dieksekusi karna array data hanya memiliki 3 elemen
+	}
+	cout << "Baris Program yang terakhir" << endl;
+	//penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	return 0;
 }
