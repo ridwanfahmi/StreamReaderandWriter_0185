dccc213105461b8ea14016b41bfbb224cefca114
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-04 10:58:29 +0700

    menyelesaikan project ketiga

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 int main()
 {
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuah int maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
 
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+
+	}
+	catch (...) {
+		//jika selain int maka block ini akan dieksekusi
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+	return 0;
 }
