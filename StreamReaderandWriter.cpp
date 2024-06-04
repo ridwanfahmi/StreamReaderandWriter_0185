92bd91a8fa76db83cd40877f7b5478c5572a38d7
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-04 10:19:39 +0700

    kalau memasukan q maka looping berhenti

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -21,6 +21,10 @@ int main() {
 
 	//unlimited loop untuk menulis
 	while (true) {
-		
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
 	}
 }
